35767915aa07e30a765299fe2e2f68a9031eac93
SaKu2110 <shotaro106@icloud.com>
2019-03-17 15:50:20 +0900

    first commit

---
+++ translate.rb
@@ -0,0 +1,77 @@
+class Brainf_ck
+
+  def initialize(src)
+    # ソースコードを文字単位で分解
+    @tokens = src.chars.to_a
+    # ジャンプの位置を特定する
+    @jumps = identify_jump_point(@tokens)
+  end
+
+  def decode
+    pointer = []
+    process_num = 0
+    pointer_num = 0
+
+    # 命令文の処理
+    while process_num < @tokens.size
+      case @tokens[process_num]
+      when "+"
+        pointer[pointer_num] ||= 0
+        pointer[pointer_num] += 1
+      when "-"
+        pointer[pointer_num] ||= 0
+        pointer[pointer_num] -= 1
+      when ">"
+        pointer_num += 1
+      when "<"
+        pointer_num -= 1
+        # ポインターの番号が負の時のエラー処理
+      when "["
+        if pointer[pointer_num] == 0
+          process_num = @jumps[process_num]
+        end
+      when "]"
+        if pointer[pointer_num]!= 0
+          process_num = @jumps[process_num]
+        end
+      when "."
+        period = (pointer[pointer_num] || 0)
+        print period.chr
+      when ","
+        pointer[pointer_num] = $stdin.getc.ord
+      end
+
+      # 処理が完了したら１増やす
+      process_num += 1
+    end
+  end
+
+  private
+
+  def identify_jump_point(tokens)
+    # ジャンプの始点と終点を記録する
+    jump_points = {}
+    # [ の位置を記録する
+    start_points = []
+
+    tokens.each_with_index do |str, point|
+      if str == "["
+        start_points.push(point)
+      elsif str == "]"
+        # ] が多い時のエラー処理
+
+        from = start_points.pop
+        to = point
+
+        jump_points[from] = to
+        jump_points[to] = from
+      end
+    end
+    # [ が多い時のエラー処理
+
+    jump_points
+  end
+
+end
+
+Brainf_ck.new(ARGF.read).decode
